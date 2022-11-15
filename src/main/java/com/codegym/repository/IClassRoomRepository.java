38139cd70a04b6dfdc7c858ed624997d759a881c
Compass <ndphong165@gmail.com>
2022-11-15 12:06:01 +0700

    first commit

---
+++ src/main/java/com/codegym/repository/IClassRoomRepository.java
@@ -0,0 +1,9 @@
+package com.codegym.repository;
+
+import com.codegym.model.ClassRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IClassRoomRepository extends JpaRepository<ClassRoom, Long> {
+
+
+}
